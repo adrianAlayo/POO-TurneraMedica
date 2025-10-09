8189461a8f5159f327b1f7b7e2586a5d1b9fd332
patrick <pa1180@gmail.com>
2025-10-09 17:29:23 -0300

    cambio provisionales

--- Turnera Medica -TP Final/GUI/Login.cs
+++ Turnera Medica -TP Final/GUI/Login.cs
@@ -16,5 +16,10 @@ namespace Turnera_Medica__TP_Final.GUI
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
