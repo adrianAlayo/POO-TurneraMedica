6d0aa59a1a7037bdd2e89a64bd83e9ae15eecfa7
Matias2024777 <m.minaya2807@gmail.com>
2025-10-16 14:52:24 -0300

    gui de cambiar contraseña

--- Turnera Medica -TP Final/GUI/UpdatePassword.cs
+++ Turnera Medica -TP Final/GUI/UpdatePassword.cs
@@ -16,5 +16,15 @@ namespace Turnera_Medica__TP_Final.GUI
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
