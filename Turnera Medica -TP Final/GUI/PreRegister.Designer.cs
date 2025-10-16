bca1f3906073075e22fda595df665a67d76e205f
adrianAlayo <axelalayo8@gmail.com>
2025-10-16 16:17:04 -0300

    Terminando la ventana de "RegisterMedico" solamente me falta comprobar la informacion par aposteriormente guardarlo en la Base de Datos

--- Turnera Medica -TP Final/GUI/PreRegister.Designer.cs
+++ Turnera Medica -TP Final/GUI/PreRegister.Designer.cs
@@ -101,6 +101,7 @@
             this.Controls.Add(this.label1);
             this.Name = "PreRegister";
             this.Text = "PreRegister";
+            this.Load += new System.EventHandler(this.PreRegister_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Turnera Medica -TP Final/GUI/RegisterMedic.cs
+++ Turnera Medica -TP Final/GUI/RegisterMedic.cs
@@ -16,5 +16,43 @@ namespace Turnera_Medica__TP_Final.GUI
         {
             InitializeComponent();
         }
+
+        private void lblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Volver_login_Click(object sender, EventArgs e)
+        {
+            //Llamo el form o ventana Login
+            PreRegister PreRegister_form = new PreRegister();
+            PreRegister_form.Show(); //Abre este form
+            this.Hide(); // Oculto el Login
+        }
+
+        private void RegisterM_send_Click(object sender, EventArgs e)
+        {
+            //Aca hay que hacer toda la logica de como ver la informacion si es correcta para poder guardarla en a base de datos
+        }
     }
 }
