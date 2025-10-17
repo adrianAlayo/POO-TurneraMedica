a8d02fa2933b3df26b7c0a0fc0d4faf49b6efeb9
Matias2024777 <m.minaya2807@gmail.com>
2025-10-17 13:27:19 -0300

    agregamos botones de volver a las vistas de ver todos los turnos y por turno especifico para el medico

--- Turnera Medica -TP Final/GUI/GUI_Medico/M_Shifts.Designer.cs
+++ Turnera Medica -TP Final/GUI/GUI_Medico/M_Shifts.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.return_start_M = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // return_start_M
+            // 
+            this.return_start_M.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.return_start_M.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.return_start_M.Location = new System.Drawing.Point(326, 398);
+            this.return_start_M.Name = "return_start_M";
+            this.return_start_M.Size = new System.Drawing.Size(150, 40);
+            this.return_start_M.TabIndex = 0;
+            this.return_start_M.Text = "Volver";
+            this.return_start_M.UseVisualStyleBackColor = false;
+            this.return_start_M.Click += new System.EventHandler(this.return_start_M_Click);
+            // 
+            // M_Shifts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.return_start_M);
+            this.Name = "M_Shifts";
             this.Text = "Shifts";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button return_start_M;
     }
 }
