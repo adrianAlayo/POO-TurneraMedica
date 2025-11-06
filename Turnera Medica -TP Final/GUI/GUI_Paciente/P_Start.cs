60b1c035437388e72efea9743bdc57481434d0b9
Matias2024777 <m.minaya2807@gmail.com>
2025-11-06 14:37:20 -0300

    .

--- Turnera Medica -TP Final/GUI/GUI_Paciente/P_Start.cs
+++ Turnera Medica -TP Final/GUI/GUI_Paciente/P_Start.cs
@@ -15,7 +15,7 @@ namespace Turnera_Medica__TP_Final.GUI.GUI_Paciente
     public partial class P_Start : Form
     {
         private Paciente userpatient { get; set; }
-        public P_Start(Paciente userpatient )
+        public P_Start(Paciente userpatient)
         {
             InitializeComponent();
             this .userpatient = userpatient;
