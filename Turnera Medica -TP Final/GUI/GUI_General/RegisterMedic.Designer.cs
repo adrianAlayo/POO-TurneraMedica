a5ec4f801d47f30d3b59fc6604558a2f8fb9e008
Matias2024777 <m.minaya2807@gmail.com>
2025-11-26 21:48:15 -0300

    a

--- Turnera Medica -TP Final/GUI/GUI_General/RegisterMedic.Designer.cs
+++ Turnera Medica -TP Final/GUI/GUI_General/RegisterMedic.Designer.cs
@@ -76,7 +76,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(219, 9);
+            this.label1.Location = new System.Drawing.Point(215, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(400, 52);
             this.label1.TabIndex = 7;
